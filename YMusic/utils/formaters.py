ccb6c037a883cf33132547c712306f972c2e616e
Botz Hub <botzhub01@gmail.com>
2025-06-18 09:39:59 +0530

    Update formaters.py

--- YMusic/utils/formaters.py
+++ YMusic/utils/formaters.py
@@ -1,3 +1,17 @@
+import math
+import time
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
 def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
